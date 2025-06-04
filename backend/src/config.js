5dd6c8b83af30bcf6b943cefa7ab6d159dd9b673
Dimitri A. Matveev <dimitri.matveev@icloud.com>
2025-06-04 12:08:24 +0300

    Удаление скомпилированных файлов фронтенда (`index.html` и стили) из сборки.

--- backend/src/config.js
+++ backend/src/config.js
@@ -4,17 +4,15 @@ const dotenv = require("dotenv");
 // Инициализация dotenv
 dotenv.config();
 
-// Получение __dirname для ES модулей
-
 // Конфигурация приложения
 const CONFIG = {
   PORT: process.env.PORT || 3013,
   PATHS: {
-    BUILD_DIR: resolve(__dirname, "../../frontend/dist"),
+    BUILD_DIR: resolve(__dirname, "../../frontend/build"),
     IMAGES_DIR: resolve(__dirname, "../../../../images"),
     DATABASE_DIR: resolve(__dirname, "../../../../database"),
     DATABASE_FILE: resolve(__dirname, "../../../../database/projects.json"),
-    INDEX_HTML: join(resolve(__dirname, "../../frontend/dist"), "index.html"),
+    INDEX_HTML: join(resolve(__dirname, "../../frontend/build"), "index.html"),
   },
 };
 
