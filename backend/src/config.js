54ede30fb82686e7aea12229befa8e6b7e6c8829
Dimitri A. Matveev <dimitri.matveev@icloud.com>
2025-06-04 13:14:54 +0300

    Обновление путей к директориям `images` и `database` в конфигурационном файле backend.

--- backend/src/config.js
+++ backend/src/config.js
@@ -9,10 +9,13 @@ const CONFIG = {
   PORT: process.env.PORT || 3013,
   PATHS: {
     BUILD_DIR: resolve(__dirname, "../../frontend/build"),
-    IMAGES_DIR: resolve(__dirname, "../../../../images"),
-    DATABASE_DIR: resolve(__dirname, "../../../../database"),
-    DATABASE_FILE: resolve(__dirname, "../../../../database/projects.json"),
     INDEX_HTML: join(resolve(__dirname, "../../frontend/build"), "index.html"),
+    // IMAGES_DIR: resolve(__dirname, "../../../../images"),
+    // DATABASE_DIR: resolve(__dirname, "../../../../database"),
+    // DATABASE_FILE: resolve(__dirname, "../../../../database/projects.json"),
+    IMAGES_DIR: resolve(__dirname, "../../images"),
+    DATABASE_DIR: resolve(__dirname, "../../database"),
+    DATABASE_FILE: resolve(__dirname, "../../database/projects.json"),
   },
 };
 
