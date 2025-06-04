a18e825de9142fb9ced9dfcf90e80056ed76d3dc
Dimitri A. Matveev <dimitri.matveev@icloud.com>
2025-06-04 12:16:30 +0300

    Удаление скомпилированных файлов фронтенда и конфигурационного файла ESLint.

--- backend/ecosystem.config.js
+++ backend/ecosystem.config.js
@@ -17,7 +17,7 @@ module.exports = {
       repo: "git@github.com:nekrasovka-library/csm.nekrasovka.git",
       path: "/var/www/csm.test.nekrasovka",
       "post-deploy":
-        "cd /var/www/csm.test.nekrasovka/current/frontend && npm install && cd ../backend && npm install && pm2 reload /var/www/csm.test.nekrasovka/current/backend/ecosystem.config.js --env production",
+        "cd /var/www/csm.test.nekrasovka/current/frontend && npm install && npm run build && cd ../backend && npm install && pm2 reload /var/www/csm.test.nekrasovka/current/backend/ecosystem.config.js --env production",
       env: {
         NODE_ENV: "production",
         PORT: 3013,
