4072f7dc952f9d7b6dd6a775aee879e33fbc4625
Dimitri A. Matveev <dimitri.matveev@icloud.com>
2025-06-04 11:23:22 +0300

    Добавление скомпилированных файлов фронтенда (`index.html` и стили) в сборку.

--- backend/src/config.js
+++ backend/src/config.js
@@ -1,6 +1,6 @@
-import { fileURLToPath } from 'url';
-import { resolve, join, dirname } from 'node:path';
-import dotenv from 'dotenv';
+import { fileURLToPath } from "url";
+import { resolve, join, dirname } from "node:path";
+import dotenv from "dotenv";
 
 // Инициализация dotenv
 dotenv.config();
@@ -13,12 +13,15 @@ const __dirname = dirname(__filename);
 const CONFIG = {
   PORT: process.env.PORT,
   PATHS: {
-    BUILD_DIR: resolve(__dirname, '../../frontend/dist'),
-    IMAGES_DIR: resolve(__dirname, '../../images'),
-    DATABASE_DIR: resolve(__dirname, '../../database'),
-    DATABASE_FILE: resolve(__dirname, '../../database/projects.json'),
-    INDEX_HTML: join(resolve(__dirname, '../../frontend/dist'), 'index.html')
-  }
+    BUILD_DIR: resolve(__dirname, "../../../frontend/dist"),
+    IMAGES_DIR: resolve(__dirname, "../../../images"),
+    DATABASE_DIR: resolve(__dirname, "../../../database"),
+    DATABASE_FILE: resolve(__dirname, "../../../database/projects.json"),
+    INDEX_HTML: join(
+      resolve(__dirname, "../../../frontend/dist"),
+      "index.html",
+    ),
+  },
 };
 
 export default CONFIG;
