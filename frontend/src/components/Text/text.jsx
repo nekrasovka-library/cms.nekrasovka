b1196b2303501b8e0bbf885d22fcc42bcb0913f1
Dimitri A. Matveev <dimitri.matveev@icloud.com>
2025-07-08 15:48:54 +0300

    Рефакторинг

--- frontend/src/components/Text/text.jsx
+++ frontend/src/components/Text/text.jsx
@@ -2,13 +2,14 @@ import React from "react";
 import { TextContainer } from "./text.styles.js";
 
 const Text = ({ text, backgroundColor, textAlign, gap, tracks }) => {
+  console.log("❗", text);
   return (
     <TextContainer
       $backgroundColor={backgroundColor}
       $textAlign={textAlign}
       $gap={gap}
       $tracks={tracks}
-      dangerouslySetInnerHTML={{ __html: text }}
+      dangerouslySetInnerHTML={{ __html: text.join("") }}
     />
   );
 };
