3c078f8481bf9e52e8cbbae4fd5ec7bdbfa04af3
Dimitri A. Matveev <dimitri.matveev@icloud.com>
2025-07-14 09:49:32 +0300

    Рефакторинг: удалены устаревшие компоненты "Preview", оптимизирована структура создания блоков, упрощены стили "skeleton-card", доработана изоляция логики между "Constructor" и "Preview".

--- backend/ecosystem.config.js
+++ backend/ecosystem.config.js
@@ -10,7 +10,7 @@ module.exports = {
         FONTS: "../../../fonts",
         DATABASE: "../../../database",
         FRONTEND: "../../frontend/build",
-        FILES: "files",
+        FILES: "../../../files",
       },
     },
   ],
